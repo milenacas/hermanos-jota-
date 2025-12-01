b152481b4f636b7658e08adf65b861ee5d85424f
Milena Casarrubia <122313670+milenacas@users.noreply.github.com>
2025-12-01 00:32:39 -0300

    Update API URL for fetching products

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -24,7 +24,7 @@ function DetalleProducto({
     const fetchProductos = async () => {
       setLoading(true);
       try {
-        const res = await fetch(`${import.meta.env.VITE_API_URL}/api/productos`);
+        const res = await fetch(`${process.env.REACT_APP_API_URL}/api/productos`);
 
         if (!res.ok)
           throw new Error("Error al cargar productos desde el backend");
